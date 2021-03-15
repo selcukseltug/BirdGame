b393acb58f1e2c7744ba971d3919489a7acd7ec5
selcukseltug <69910111+selcukseltug@users.noreply.github.com>
2021-03-15 20:42:04 +0300

    Initial commit

---
+++ android/src/com/selcukseltug/birdgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.selcukseltug.birdgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.selcukseltug.birdgame.BirdGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BirdGame(), config);
+	}
+}
